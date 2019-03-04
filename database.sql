dae153f992a8c7417405d9d881add6824a747168
Olivia Mattsson <oliviamattssons@gmail.com>
2019-03-04 15:06:22 +0100

    Added picture of E/R model and started on sql-code

---
+++ database.sql
@@ -0,0 +1,41 @@
+PRAGMA foreign_keys=OFF;
+
+DROP IF EXISTS customers;
+DROP IF EXISTS orders;
+DROP IF EXISTS recipies;
+DROP IF EXISTS cookies;
+
+PRAGMA foreign_keys=ON;
+
+
+CREATE TABLE customers (
+customer_name  text,
+
+
+
+);
+
+
+CREATE TABLE orders (
+
+
+
+    
+);
+
+
+
+CREATE TABLE recipies (
+
+
+
+    
+);
+
+
+CREATE TABLE cookies (
+
+
+
+    
+);
